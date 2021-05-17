f030a0d13eca47ce5029c774bb74f008ea53c9df
samuraiprogramming-eng <sohaibfathy33@gmail.com>
2021-05-17 22:08:14 +0200

    Agency

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  /* navbar toggler */
+
+  $('.navbar-toggler').html("<i class='fa fa-arrow-down fa-2x' style='color:#f15025'></i>");
+
+  // smooth scroll
+
+// *************** set date ****************
+const date = (document.getElementById('date')
+.innerHTML = new Date().getFullYear());
+
+
+
+
+
+});
